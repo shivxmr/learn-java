e9aae0f0a91636c0f9a20e80267c4b1c0449617f
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-03-11 23:47:41 +0530

    Initial Commit

---
+++ src/shivam/Main.java
@@ -0,0 +1,22 @@
+package shivam;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello Java");
+//    String name = "Shivam";
+//    String myname = name;
+//        System.out.println(myname);
+//        System.out.println(name);
+
+/* Primitive Data Types
+        long number = 12354678809L;
+
+*/
+    }
+}
+
+
+
+
+
+
