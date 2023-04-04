0ca514458529b04f12cb9929fcbea75102c534a2
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-04-05 01:31:42 +0530

    Initial Commit

--- src/shivam/Main.java
+++ src/shivam/Main.java
@@ -1,7 +1,8 @@
 package shivam;
 
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
 //        System.out.println("Hello Java");
 //    String name = "Shivam";
 //    String myname = name;
@@ -10,11 +11,22 @@ public class Main {
 
 /* Primitive Data Types
         long number = 12354678809L;
-
 */
-    }
-}
+// Non-primitive Data Types
 
+       /* String name = "Shivam";
+        System.out.println(name.length());
+        System.out.println(name.charAt(2));
+        System.out.println(name.replace('a', 'i')); */
+
+        // Important - Strings are immutable in JAVA.
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter your Roll No. : ");
+            int roll = input.nextInt();
+            System.out.println("Your roll no. is " + roll);
+        }
+}
 
 
 
---
+++ src/shivam/roll.java
@@ -0,0 +1,12 @@
+package shivam;
+
+import java.util.Scanner;
+
+public class roll {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your roll ");
+        int roll = input.nextInt();
+        System.out.println("Your roll no. is " + roll);
+    }
+}
