e6c7807e23a80885109664bbbdee1f100e014eee
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-04-07 16:57:55 +0530

    TypeCasting

---
+++ src/shivam/LoopsSyntax.java
@@ -0,0 +1,30 @@
+package shivam;
+
+import java.util.Scanner;
+
+public class LoopsSyntax {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+
+
+    //Take input from user and print next 5 integers
+/*
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int num = input.nextInt();
+        int num1 = num+5;
+        int i = 0;
+        for (i=num+1; i<=num1; i+= 1) {
+            System.out.println(i);
+        }
+    }
+ */
+
+}
