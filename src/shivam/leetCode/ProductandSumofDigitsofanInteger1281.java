7ef4eb708c3d4942886eaa23fec9037df0d23958
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-05-20 18:38:54 +0530

    20 May

---
+++ src/shivam/leetCode/ProductandSumofDigitsofanInteger1281.java
@@ -0,0 +1,29 @@
+package shivam.leetCode;
+
+// Very Easy
+// https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+
+import java.util.Scanner;
+
+public class ProductandSumofDigitsofanInteger1281 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Input: ");
+        int inp = input.nextInt();
+        int mul = 1;
+        int addi = 0;
+        int r = 0;
+
+        while (inp > 0) {
+            r = inp % 10;
+            mul = mul * r;
+            addi = addi + r;
+            inp = inp / 10;
+        }
+
+        System.out.println(mul-addi);
+    }
+
+}
