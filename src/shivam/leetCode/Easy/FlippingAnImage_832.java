650ea84da4e77e888b93f14eef0684a912826ab1
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-06-01 15:54:35 +0530

    FlippingAnImage_832

---
+++ src/shivam/leetCode/Easy/FlippingAnImage_832.java
@@ -0,0 +1,21 @@
+package shivam.leetCode.Easy;
+
+import java.util.Arrays;
+
+public class FlippingAnImage_832 {
+    public static void main(String[] args) {
+        int[][] image = {{1,1,0},{1,0,1},{0,0,0}};
+        System.out.println(Arrays.deepToString(flipAndInvertImage(image)));
+    }
+
+    static int[][] flipAndInvertImage(int[][] image) {
+        for (int[] row: image) {
+            for (int i = 0; i < (image[0].length+1)/2; i++) {
+                int temp = row[i] ^ 1;
+                row[i] = row[image[0].length - i - 1] ^ 1;
+                row[image[0].length - i - 1] = temp;
+            }
+        }
+        return image;
+    }
+}
