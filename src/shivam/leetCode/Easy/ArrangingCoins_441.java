564e7789cf9d451d1192cc05311918e185f04bc3
Shivam Raj <shivamraj.me21@rvce.edu.in>
2023-08-02 10:41:37 +0530

    Maze Problems (Backtracking)

---
+++ src/shivam/leetCode/Easy/ArrangingCoins_441.java
@@ -0,0 +1,17 @@
+package shivam.leetCode.Easy;
+
+//https://leetcode.com/problems/arranging-coins/
+
+public class ArrangingCoins_441 {
+    public static void main(String[] args) {
+        int n = 5;
+        arrangeCoins(n);
+    }
+
+    static int arrangeCoins(int n) {
+
+
+
+        return n;
+    }
+}
